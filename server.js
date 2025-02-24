eb6bb69cfcba02ee9ccd03f82f8ca5cf930d7f8b
chtiemcz <xtimech.kali@gmail.com>
2025-02-24 22:03:43 +0100

    translate + def tru

--- server.js
+++ server.js
@@ -1,19 +1,27 @@
-// вызов модуля imap
+// Developer - Ivan 💲
+// Github - https://github.com/chtiemcz
+// Этот скрипт поможет более быстрее понять модуль imap от node.js, благодаря примеру и комментариям.
+// Сделано с душой ❤️‍🩹
+
+
+// Вызов модуля imap, util, dotenv
+require('dotenv').config();
 const Imap = require('imap');
 const { inspect } = require('util');
 
-// данные для входа в IMAP 
+
+// Данные для входа в IMAP 
 const imap = new Imap ({
-  user: 'matthewmaxwell2014@personarsmail.ru',
-  password: 'zrfkbkaqS4785',
-  host: 'imap.firstmail.ltd',
-  port: 993,
+  user: process.env.EMAIL_USER,   // Почта (берется с файла .env)
+  password: process.env.EMAIL_PASSWORD,   // Пароль (берется с файла .env)
+  host: 'imap.firstmail.ltd',  // Хост подключения
+  port: 993,   // Порт подключения
   tls: true,
-  connTimeout: 5000,  // Устанавливаем тайм-аут подключения в 5 секунд
-  authTimeout: 5000,  // Тайм-аут аутентификации
+  connTimeout: 5000,    // Устанавливаем тайм-аут подключения в 5 секунд
+  authTimeout: 5000,   // Тайм-аут аутентификации
 });
 
-// открытие бокса куда приходят соо
+// Открытие бокса, куда приходят сообщения
 function openInbox(cb) {
   imap.openBox('INBOX', true, cb);
 }
@@ -21,12 +29,12 @@ function openInbox(cb) {
 imap.once('ready', function() {
   openInbox(function(err, box) {
     if (err) throw err;
-    var f = imap.seq.fetch('1', {
+    var f = imap.seq.fetch('1:*', {
       bodies: ['TEXT', ('HEADER.FIELDS (FROM TO SUBJECT DATE)')],
       struct: true
     });
     f.on('message', function(msg, seqno) {
-      console.log('Message #%d', seqno);
+      console.log('Сообщение #%d', seqno);
       var prefix = '(#' + seqno + ') ';
       msg.on('body', function(stream, info) {
         var buffer = '';
@@ -34,32 +42,34 @@ imap.once('ready', function() {
           buffer += chunk.toString('utf8');
         });
         stream.once('end', function() {
-          console.log(prefix + 'Parsed header: %s', inspect(Imap.parseHeader(buffer)));
+          console.log(prefix + 'Парсинг заголовка: %s', inspect(Imap.parseHeader(buffer)));
         });
       });
       msg.once('attributes', function(attrs) {
-        console.log(prefix + 'Attributes: %s', inspect(attrs, false, 8));
+        console.log(prefix + 'Атрибуты: %s', inspect(attrs, false, 8));
       });
       msg.once('end', function() {
-        console.log(prefix + 'Finished');
+        console.log(prefix + 'Готово');
       });
     });
     f.once('error', function(err) {
-      console.log('Fetch error: ' + err);
+      console.log('Ошибки: ' + err);
     });
     f.once('end', function() {
-      console.log('Done fetching all messages!');
+      console.log('Проверены все письма!');
       imap.end();
     });
   });
 });
 
+
 imap.once('error', function(err) {
   console.log(err);
 });
 
+// конец подключения
 imap.once('end', function() {
-  console.log('Connection ended');
+  console.log('Подключение закончено.');
 });
 
 imap.connect();
